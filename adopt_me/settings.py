c6c85e3f61c822f116611d9f7f61b1802ed0e648
erincunningham7 <cunninnghamerin@gmail.com>
2024-03-09 20:45:14 +0000

    update code for deployment

--- adopt_me/settings.py
+++ adopt_me/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*8eiqs2m9jekd7y8u=649^4%%8-=f9aos3&ixw$c%_dr)@kg%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-erincunning-pp4projectp-8m390i6kicz.ws-eu108.gitpod.io"]
+ALLOWED_HOSTS = ["8000-erincunning-pp4projectp-8m390i6kicz.ws-eu108.gitpod.io",".herokuapp.com"]
 
 
 # Application definition
