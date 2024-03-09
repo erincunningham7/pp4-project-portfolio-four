6be32e42f3092d0b68d69948d258f87449169540
erincunningham7 <cunninnghamerin@gmail.com>
2024-03-09 21:56:55 +0000

    create Advert model

--- adopt_me/settings.py
+++ adopt_me/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'ads',
 ]
 
@@ -88,6 +89,11 @@ WSGI_APPLICATION = 'adopt_me.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Advert(models.Model):
+    title = models.CharField(max_length=254, unique=True)
+    excerpt = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pet_name = models.CharField(max_length=254, null=False, blank=False)
+    pet_breed = models.CharField(max_length=254, null=False, blank=False)
+    pet_age = models.IntegerField()
+    vaccinated = models.BooleanField(default=False)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
