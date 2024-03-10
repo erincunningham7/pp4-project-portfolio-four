196e3aff8c50f2fe972cfbf1f75806c3a3f7bd56
erincunningham7 <cunninnghamerin@gmail.com>
2024-03-10 21:46:26 +0000

    change ads url path

--- adopt_me/urls.py
+++ adopt_me/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("ads/", include("ads.urls")),
+    path('', include('ads.urls'), name='ads-urls'),
 ]
