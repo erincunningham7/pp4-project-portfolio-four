e460390d7a37c1121c0131330281324395842711
erincunningham7 <cunninnghamerin@gmail.com>
2024-03-13 18:23:11 +0000

    enable serving of image files

--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Advert(models.Model):
     title = models.CharField(max_length=254, unique=True)
     excerpt = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = CloudinaryField('image', default='placeholder')
     pet_name = models.CharField(max_length=254, null=False, blank=False)
     pet_breed = models.CharField(max_length=254, null=False, blank=False)
     pet_age = models.IntegerField()
